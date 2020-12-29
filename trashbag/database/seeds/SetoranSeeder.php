864d8d5ecf53a11fd46c7263055b51bff7e768a7
Juang999 <juggernaut.9909@g,ail.com>
2020-12-29 17:52:09 +0700

    update SetoranSeeder

--- trashbag/database/seeds/SetoranSeeder.php
+++ trashbag/database/seeds/SetoranSeeder.php
@@ -11,6 +11,38 @@ class SetoranSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('setorans')->insert([
+            [
+                'user_id' => 1,
+                'jenis_id' => 1,
+            ],[
+                'user_id' => 1,
+                'jenis_id' => 2,
+            ],[
+                'user_id' => 1,
+                'jenis_id' => 3,
+            ],[
+                'user_id' => 2,
+                'jenis_id' => 1,
+            ],[
+                'user_id' => 2,
+                'jenis_id' => 2,
+            ],[
+                'user_id' => 2,
+                'jenis_id' => 3,
+            ],[
+                'user_id' => 1,
+                'jenis_id' => 1,
+            ],[
+                'user_id' => 1,
+                'jenis_id' => 2,
+            ],[
+                'user_id' => 1,
+                'jenis_id' => 3,
+            ],[
+                'user_id' => 2,
+                'jenis_id' => 1,
+            ]
+        ]);
     }
 }
