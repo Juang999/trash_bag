3f3f896eb638b2c87427f65ff16bfd77bbe69de3
Juang999 <juggernaut.9909@gmail.com>
2021-01-28 10:35:33 +0700

    udpate seeders

--- trashbag/database/seeds/DatabaseSeeder.php
+++ trashbag/database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
-        $this->call(JenisSeeder::class);
-        $this->call(SetoranSeeder::class);
-        $this->call(TabunganSeeder::class);
+        // $this->call(UserSeeder::class);
+        // $this->call(JenisSeeder::class);
+        // $this->call(SetoranSeeder::class);
+        // $this->call(TabunganSeeder::class);
         $this->call(KeuanganSeeder::class);
     }
 }
