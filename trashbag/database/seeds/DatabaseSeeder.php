cdbc991671824a67558ff7f632886b3fd18b42bb
Juang999 <juggernaut.9909@gmail.com>
2021-01-28 10:38:19 +0700

    update database seeder

--- trashbag/database/seeds/DatabaseSeeder.php
+++ trashbag/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
         // $this->call(JenisSeeder::class);
         // $this->call(SetoranSeeder::class);
         // $this->call(TabunganSeeder::class);
