5c0f31cd08d8436cc1c0f57989b62aed721dfb04
ilahaka77 <ahmadarifilahaka77@gmail.com>
2021-01-28 17:25:46 +0700

    update

--- trashbag/app/Http/Controllers/SetoranController.php
+++ trashbag/app/Http/Controllers/SetoranController.php
@@ -10,11 +10,12 @@ use Illuminate\Http\Request;
 class SetoranController extends Controller
 {
     public function index(){
-        $setoran = Setoran::with('user', 'penanggungJawab', 'jenis')->get();
+        // $setoran = Setoran::with('user', 'penanggungJawab', 'jenis')->get();
+        $setoran = Setoran::with('user')->with('penanggungJawab')->with('jenis')->get();
         $jumlah = JenisSampah::select('jenissampah.id','jenis_sampah', DB::raw('sum(setorans.berat) as jumlah'))
         ->leftJoin('setorans', 'jenissampah.id', '=', 'setorans.jenis_id')
         ->groupby('jenissampah.id','jenissampah.jenis_sampah')->orderby('jenissampah.id')->get();
-        // dd($jumlah);
+        // dd($setoran);
         return view('setoran.index', ['menu'=>'setoran', 'setoran'=> $setoran, 'jumlah'=>$jumlah]);
     }
 }
